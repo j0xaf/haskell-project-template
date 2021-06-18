876d96d2e7852738a0f87a8d69d2b53e0713a1be
Jörn Gersdorf <j0xaf@j0xaf.de>
2021-06-18 20:07:56 +0200

    initial

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  inherit (pkgs) callPackage fetchFromGitHub mkShell lib;
+  ghcVersion = "8.10.4";
+
+  concatenateVersion = v: lib.concatStrings (builtins.filter (c: c != ".") (lib.stringToCharacters v));
+  ghc = lib.attrByPath [ "haskell" "compiler" "ghc${concatenateVersion ghcVersion}" ] null pkgs;
+
+  easy-hls-src = fetchFromGitHub {
+    owner  = "jkachmar";
+    repo   = "easy-hls-nix";
+    rev    = "9d64543a015563942c954b89addc1108800ed134";
+    sha256 = "sha256-33cphoKJK0GB7M7iTYwuBoXW7q6JFBo/dkLsRsYb+Os=";
+  };
+  easy-hls = callPackage easy-hls-src {
+    ghcVersions = [ ghcVersion ];
+  };
+in
+
+mkShell {
+  buildInputs = with pkgs; [
+    easy-hls
+    cabal-install
+    ghc
+  ];
+}
